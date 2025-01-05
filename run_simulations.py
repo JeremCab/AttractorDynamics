172be3808877dfc9174f44c1a952a1b3352af45c
JeremCab <42182946+JeremCab@users.noreply.github.com>
2025-01-05 14:38:56 +0100

    Add STDP simulations without trigger patterns for BGT network

--- run_simulations.py
+++ run_simulations.py
@@ -58,15 +58,15 @@ def run_grid_search():
     modes = ["stdp", "gp", "stdp-gp"]                 # 3 modes
     input_lengths = [1001]                            # fixed
     trigger_lengths = [50]                            # fixed
-    nb_triggers = [0] # [1, 3, 5, 7, 9, 11] # [10]          # several triggers
+    nb_triggers = [1, 3, 5, 7, 9, 11] # [10]          # several triggers
     seeds = [42, 79, 82, 83, 47, 49, 13, 77, 55, 15]  # 10 seeds
     temperatures = [10.0]                             # fixed
     cooling_rates = [0.995]                           # fixed
     etas = [0.1, 0.01, 0.001, 0.0001] # [0.025]            # 4 etas # XXX XXX XXX
     plumbs = [1.0]                                    # fixed
     bounds = ["minus0.4999,0.4999"]   #["minus0.4999,1.4999"]                  # fixed
-    noises = [0.25] # [0.3]                           # fixed
-    network = ["random"]  # ["bgt"]              # fixed
+    noises = [0.25]                                   # fixed
+    network = ["bgt"] # ["random"]                    # fixed
 
     # Generate all combinations of parameters using itertools.product
     param_grid = itertools.product(modes, input_lengths, trigger_lengths, nb_triggers,
